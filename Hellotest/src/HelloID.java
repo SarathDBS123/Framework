e82c66b0e28bf2856514d933b163d76aa5325dbf
SarathDBS123 <sarath1.madineni@gmail.com>
2019-02-09 09:57:48 +0530

    first commit and push

---
+++ Hellotest/src/HelloID.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+public class HelloID {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.ie.driver", "E://Selenium Software//IEDriverServer.exe");
+		WebDriver driver = new InternetExplorerDriver();
+		
+		driver.get("https://www.google.com/");
+		//E:\Selenium Software
+		System.out.println("Sarath testing");
+		driver.getTitle();
+		driver.close();
+		
+	}
+	
+	int [] x = new int [3];
+	
+}
